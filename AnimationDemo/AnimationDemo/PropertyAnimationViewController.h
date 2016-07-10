04c46b9064b08946be055958d6bf04535bd9a1f4
snmsw <sn_msw@163.com>
2016-07-10 15:28:32 +0800

    pop的demo
    pop动画的简单应用

---
+++ AnimationDemo/AnimationDemo/PropertyAnimationViewController.h
@@ -0,0 +1,13 @@
+//
+//  PropertyAnimationViewController.h
+//  AnimationDemo
+//
+//  Created by king on 16/7/5.
+//  Copyright © 2016年 songs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PropertyAnimationViewController : UIViewController
+
+@end
